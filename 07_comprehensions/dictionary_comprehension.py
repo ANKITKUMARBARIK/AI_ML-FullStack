d9570a597a1774457623f5d0fb7dd0ab8665619e
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-08-14 14:49:12 +0530

    Add dictionary comprehension example for converting tea prices from INR to USD

---
+++ 07_comprehensions/dictionary_comprehension.py
@@ -0,0 +1,11 @@
+"""
+
+{ expression for item in iterable if condition }
+
+"""
+
+tea_prices_inr = {"Masala chai": 40, "Green Tea": 50, "Lemon Tea": 200}
+
+tea_prices_usd = {tea: price / 80 for tea, price in tea_prices_inr.items()}
+# tea_prices_usd = {tea: f"{price/80}" for tea, price in tea_prices_inr.items()}
+print(tea_prices_usd)
