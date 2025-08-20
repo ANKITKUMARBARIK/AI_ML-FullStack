dbc0a5f7959a59015c1fb9142a3570169ba3b8dc
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-08-20 16:23:49 +0530

    Add method resolution order example with multiple inheritance

---
+++ 10_oops/method_resolution_order.py
@@ -0,0 +1,21 @@
+class A:
+    label = "A: Base class"
+
+
+class B(A):
+    label = "B: Masala blend"
+
+
+class C(A):
+    label = "C: Herbal blend"
+
+
+class D(B, C):
+    # class D(C,B):
+    pass
+
+
+cup = D()
+print(cup.label)
+
+print(D.__mro__)
